3d91ee8cd904174765aad07b3fa85112bdf5b4f2
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-08 21:49:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const increaseRankBy = (n) => {
   const items = document.querySelectorAll('ul.ranked-list li')
   
   for (let i of items) {
-    items[i].innerHTML
+    items[i].innerHTML = parseInt()
   }
 }
