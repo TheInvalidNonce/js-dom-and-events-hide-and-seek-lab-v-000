e7342e25654b5ae338b49130c576272d14fa252e
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-08 21:58:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+const getFirstSelector = (selector) => {
+  return document.querySelector(selector)
+}
+
+const nestedTarget = () => {
+  return document.getElementById('nested')
+}
+
+const increaseRankBy = (n) => {
+  const items = document.querySelectorAll('ul.ranked-list li')
+  
+  for (let i in items) {
+    items[i].innerHTML = parseInt(items[i].innerHTML) + n
+  }
+}
+
+const deepestChild = () => {
+  return document.querySelector('div#grand-node div div div div')
+}
