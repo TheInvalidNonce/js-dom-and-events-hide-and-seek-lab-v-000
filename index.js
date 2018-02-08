03d46d7159107efdd302fa500f2914f2de74840b
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-08 21:52:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const nestedTarget = () => {
 const increaseRankBy = (n) => {
   const items = document.querySelectorAll('ul.ranked-list li')
   
-  for (let i of items) {
+  for (let i in items) {
     items[i].innerHTML = parseInt(items[i].innerHTML, 10) + n
   }
 }
