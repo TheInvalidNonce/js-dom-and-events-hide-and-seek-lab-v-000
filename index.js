15fb76acab75c6715f77ec335d290397400db5c4
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-08 21:55:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ const increaseRankBy = (n) => {
 }
 
 const deepestChild = () => {
-  return document.querySelector('div#grand-node').children
+  return document.querySelector('div#grand-node').children[0]
 }
